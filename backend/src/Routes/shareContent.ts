5cbb2ccbb8abd1dc8ea0717344a4aaca5d88a6e9
amityadav0612 <amity7205@gmail.com>
2025-08-04 23:32:53 +0530

    init

---
+++ backend/src/Routes/shareContent.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { authMiddleware } from "../middleware/auth";
+
+const shareRouter=Router();
+
+shareRouter.post("/share",authMiddleware,(req,res)=>{
+
+    const share=req.body.share;
+
+    
+})
